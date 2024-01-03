e8432b066c0793f1f64142a248ea967f738dd51e
Evgeniy-Popov-98 <evgeniy6183@gmail.com>
2024-01-03 23:16:02 +0200

    Add-task#3

---
+++ js/task-3.js
@@ -0,0 +1,11 @@
+const userName = document.querySelector('#name-input');
+const newSpan = document.querySelector('#name-output');
+
+userName.addEventListener('input', onInput);
+
+function onInput() {
+  const name = userName.value.trim();
+  if (name) {
+    newSpan.innerHTML = `${name}`;
+  }
+}
