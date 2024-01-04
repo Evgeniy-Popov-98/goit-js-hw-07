942e485f9505750b8b9a82d462605d7943fa2c5f
Evgeniy-Popov-98 <evgeniy6183@gmail.com>
2024-01-04 19:32:52 +0200

    Add-Task#5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const clickMouse = document.querySelector('.change-color');
+const bodyBg = document.querySelector('body');
+const newSpan = document.querySelector('.color');
+
+clickMouse.addEventListener('click', newBackg);
+
+function newBackg() {
+  bodyBg.style.backgroundColor = getRandomHexColor();
+  newSpan.innerHTML = `${getRandomHexColor()}`;
+}
