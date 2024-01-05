b0f96ad3d7846bcb8f72f50fd7d0028a0e4dbcbf
Evgeniy-Popov-98 <143257644+Evgeniy-Popov-98@users.noreply.github.com>
2024-01-05 15:33:03 +0200

    Add-task#6

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,51 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const buttonCreate = document.querySelector('button[data-create]');
+const buttonDestroy = document.querySelector('button[data-destroy]');
+const inputNumber = document.querySelector('input');
+const addInDiv = document.querySelector('#boxes');
+
+// inputNumber.addEventListener('blur', newdiv);
+buttonCreate.addEventListener('click', checkAmount);
+buttonDestroy.addEventListener('click', destroyBoxes);
+
+function checkAmount() {
+  const amount = inputNumber.value;
+  console.log(amount);
+}
+
+function addDiv() {
+  const amount = inputNumber.value;
+  console.log(amount);
+  if (amount >= 1 && amount <= 100) {
+    let width = 30;
+    let height = 30;
+    for (let index = 0; index < amount; index++) {
+      const newDiv = document.createElement('div');
+      addInDiv.append(newDiv);
+
+      newDiv.classList.add('div-box');
+      newDiv.style.backgroundColor = getRandomHexColor();
+      newDiv.style.width = width + 'px';
+      newDiv.style.height = height + 'px';
+      newDiv.style.margin = '10px';
+
+      width += 10;
+      height += 10;
+    }
+  } else {
+    alert('Enter value from 1 to 100');
+  }
+}
+
+function destroyBoxes() {
+  const removeDiv = addInDiv.querySelector('div');
+  //   const removeDiv = addInDiv.children;
+  console.log(removeDiv);
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    removeDiv.remove();
+  }
+}
